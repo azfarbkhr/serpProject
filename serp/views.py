66e5dad7be8b061fe0abcfba1f5863804cc61c14
azfarbkhr <azfarbkhr@gmail.com>
2023-01-08 16:23:40 +0500

    setup login

--- serp/views.py
+++ serp/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate 
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
+
+# Create your views here.
+@login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the serp index.")
+    return render(request, 'serp/index.html')
--- serpProject/settings.py
+++ serpProject/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# login url
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "login" 
+
+CRISPY_TEMPLATE_PACK = 'uni_form'
